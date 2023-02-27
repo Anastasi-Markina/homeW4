7fed4ec8b96a25fde0011e59ee43ef35d3f089fc
Markina Anastasia <aanastassias@gmail.com>
2023-02-27 21:00:37 +0300

    домашка с 9го семинара

---
+++ задача9.1/Program.cs
@@ -0,0 +1,19 @@
+﻿System.Console.WriteLine("введите число M:");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+Print(m,n,0);
+
+void Print (int numM, int numN, int sum)
+{
+    sum += numN;
+    if (numN<numM)
+    {
+    
+        return;
+    }
+    
+    Print (numM, numN - 1, sum);
+
+    System.Console.Write(sum +"  ");
+}
---
+++ задача9.2/Program.cs
@@ -0,0 +1,50 @@
+﻿// Console.Write("Введите число M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// AkkermanFunction(m, n);
+
+// void AkkermanFunction (int m, int n)
+// {
+//     Console.Write (Akkerman (m, n)); 
+// }
+
+// int Akkerman(int m, int n)
+// {
+//     if (m == 0)
+//     {
+//        return n++;
+//     }
+//     else if (m > 0 && n==0)
+//     {
+//         return Akkerman(m--,1);
+    
+//     }
+//     else
+//     {
+//         return (Akkerman(m - 1, Akkerman(m, n - 1)));
+//     }
+// }
+
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+
+int functionAkkerman = Ack (m, n);
+
+Console.Write($"Функция Аккермана = {functionAkkerman} ");
+
+int Ack(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack (m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write (input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
